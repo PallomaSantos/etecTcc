4454409e65637c41de2561a8a5f85d3162b9cc35
ArthurK09 <guilhermearthur738@gmail.com>
2025-10-28 21:28:43 -0300

    Adicionando Arquivos

---
+++ App.js
@@ -0,0 +1,5 @@
+import { NavigationContainer } from "@react-navigation/native";
+import './src/navigation/AppNavigator';
+<NavigationContainer>
+  <AppNavigator></AppNavigator>
+</NavigationContainer>
